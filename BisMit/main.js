16f884eb452eb1cba9eb0987f26241402c960e5f
Hanif Audah <56548631+hanifaudah@users.noreply.github.com>
2019-11-02 07:15:19 +0700

    Add files via upload

---
+++ BisMit/main.js
@@ -0,0 +1,10 @@
+var btn = document.querySelector("#upBtn");
+btn.addEventListener("click",function(){
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+})
+
+// var gallery = docoument.querySelector(".gallery");
+// var content = docoument.querySelector(".content");
+// var gal_height = gallery.style.height;
+// var cont_height = content.style.height;
+// gal_height = "500px;"
